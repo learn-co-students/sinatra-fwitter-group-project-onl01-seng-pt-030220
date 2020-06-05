436eb0dcc555608ac00c4eb1c5664126e07fb579
github username <github email address>
2020-06-05 16:08:34 -0500

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,54 @@
 class TweetsController < ApplicationController
 
-
+    get '/tweets' do
+        authenticate 
+        @tweets = Tweet.all
+        @user = current_user
+        erb :'tweets/tweets'
+      end 
+    
+    
+      get '/tweets/new' do
+        authenticate
+        erb :'/tweets/new'
+      end
+    
+      post '/tweets' do
+        authenticate
+        @tweet = current_user.tweets.create(params[:tweet])
+        if @tweet.save
+          redirect "/tweets/#{@tweet.id}"
+        else
+          redirect '/tweets/new'
+        end
+      end
+    
+      get '/tweets/:id/edit' do 
+        @tweet = Tweet.find_by(id: params[:id])
+        authorize(@tweet)
+        erb :'/tweets/edit_tweet'
+      end 
+    
+      patch '/tweets/:id' do 
+        @tweet = Tweet.find_by(id: params[:id])
+        authorize(@tweet)
+        if @tweet.update(content: params[:content])
+          redirect '/tweets'
+        else
+          redirect "/tweets/#{@tweet.id}/edit"
+        end
+      end 
+    
+      get '/tweets/:id' do 
+        authenticate
+        @tweet = Tweet.find_by(id: params[:id])
+        erb :'/tweets/show_tweet'
+      end
+    
+      delete '/tweets/:id' do 
+        @tweet = Tweet.find_by(id: params[:id])
+        authorize(@tweet)
+        @tweet.destroy
+        redirect '/tweets'
+      end 
 end
