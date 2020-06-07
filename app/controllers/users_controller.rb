59306ca6ccf59409a5c4c02500648933140d2182
aredmond85 <aredmond85@gmail.com>
2020-06-06 20:57:20 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,14 @@ class UsersController < ApplicationController
     end
 
     post '/signup' do
-        if !params[:username].empty? || !params[:email].empty? || !params[:password].empty?
+        if params[:username] == "" || params[:email] == ""  || params[:password] == "" 
           redirect to '/signup'
         else
-          @user = User.create(params)
+          @user = User.new(username: params[:username], email: params[:email], password: params[:password])
           @user.save
           session[:user_id] = @user.id
           redirect to '/tweets'
         end
-      end
+    end
      
 end
