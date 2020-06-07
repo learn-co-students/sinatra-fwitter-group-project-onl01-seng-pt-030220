6a1dc9b1cec100948c0be226e60fc6382b6c4713
aredmond85 <aredmond85@gmail.com>
2020-06-06 22:32:30 -0500

    Done.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
 
   get '/logout' do
     if logged_in?
-      session.clear
+      session.destroy
       redirect to '/login'
     else
       redirect to '/'
