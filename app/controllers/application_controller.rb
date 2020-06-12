e43334c744a23b9af13d9617f9709de7feefa772
alelee93 <leevargasale@gmail.com>
2020-06-11 20:22:42 -0500

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,50 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get "/" do
+    erb :layout
+  end
+
+  get "/signup" do
+    erb :'/users/signup'
+  end
+
+  post "/signup" do
+    binding.pry
+    user = User.new(:email=> params[:email], :password=> params[:password], :username=> params[:username])
+    if user.save && user.email != nil
+      redirect '/tweets'
+    else
+      redirect '/failure'
+    end
+  end
+
+  get "/login" do
+    erb :'/users/login'
+  end
+
+  post "/login" do
+    user = User.find_by(:username=>params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect "/tweets"
+    # else
+    #   redirect "/failure"
+    end
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/"
+  end
+
+  helpers do
+    def logged_in?
+      !!session[:user_id]
+    end
+
+    def current_user
+        User.find(session[:user_id])
+    end
+  end
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,11 @@
 class TweetsController < ApplicationController
-
+    get "/tweets" do
+        @tweets = Tweet.all
+        if logged_in?
+            erb :'/tweets/index'
+        else 
+            redirect "/login"
+        end
+    end
 
 end
