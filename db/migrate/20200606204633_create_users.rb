84890929e7cfc8c029e06bc016275c67c220eebb
aredmond85 <aredmond85@gmail.com>
2020-06-22 10:47:00 -0500

    Done.

--- db/migrate/20200606204633_create_users.rb
+++ db/migrate/20200606204633_create_users.rb
@@ -1,9 +1,8 @@
-class CreateUsers < ActiveRecord::Migration[6.0]
+class CreateTweets < ActiveRecord::Migration[6.0]
   def change
-    create_table :users do |t|
-      t.string :username
-      t.text :email
-      t.string :password_digest
+    create_table :tweets do |t|
+      t.string :content
+      t.integer :user_id
     end
   end
 end
--- db/migrate/20200606204643_create_tweets.rb
+++ db/migrate/20200606204643_create_tweets.rb
@@ -1,8 +1,9 @@
-class CreateTweets < ActiveRecord::Migration[6.0]
+class CreateUsers < ActiveRecord::Migration[6.0]
   def change
-    create_table :tweets do |t|
-      t.text :content
-      t.integer :user_id
+    create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :password_digest
     end
   end
 end
